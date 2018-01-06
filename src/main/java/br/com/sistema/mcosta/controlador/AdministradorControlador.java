868315493688f1296b1ae0998031530df6e03a18
Pedro Pereira <pedrojr9119@gmail.com>
2018-01-06 18:49:53 -0200

    Ajuste ícone

--- src/main/java/br/com/sistema/mcosta/controlador/AdministradorControlador.java
+++ src/main/java/br/com/sistema/mcosta/controlador/AdministradorControlador.java
@@ -33,28 +33,83 @@ public class AdministradorControlador {
 	}
 	
 	@PostMapping(value = "/sobre")
-	public String salvar(@Validated Sobre sobre, Errors errors, RedirectAttributes attributes) {
+	public ModelAndView salvar(@Validated Sobre sobre, Errors errors, RedirectAttributes attributes) {
+		ModelAndView mv = new ModelAndView(CADASTRO_SOBRE);
+		
 		if (errors.hasErrors()) {
-			return CADASTRO_SOBRE;
+			return mv;
+		}
+		
+		String icone = sobre.getSobreDetalhe1().getIcone();
+		
+		if(iconeValido(icone)) {
+			sobre.getSobreDetalhe1().setIcone("fa ".concat(icone));
+		} else {
+			mv.addObject("mensagemErro", "Ícone do 1º detalhe com formato inválido!");
+			return mv;
+		}
+		
+		icone = sobre.getSobreDetalhe2().getIcone();
+		
+		if(iconeValido(icone)) {
+			sobre.getSobreDetalhe2().setIcone("fa ".concat(icone));
+		} else {
+			mv.addObject("mensagemErro", "Ícone do 2º detalhe com formato inválido!");
+			return mv;
+		}
+		
+		icone = sobre.getSobreDetalhe3().getIcone();
+		
+		if(iconeValido(icone)) {
+			sobre.getSobreDetalhe3().setIcone("fa ".concat(icone));
+		} else {
+			mv.addObject("mensagemErro", "Ícone do 3º detalhe com formato inválido!");
+			return mv;
 		}
 		
 		sobreBO.salvarSobre(sobre);
-		attributes.addFlashAttribute("mensagem", "Sobre salvo com sucesso!");
-		return "redirect:/administracao/sobre/novo";
+		mv.addObject("mensagem", "Sobre salvo com sucesso!");
+		return mv;
+	}
+	
+	private boolean iconeValido(String icone) {
+		return icone.startsWith("fa-");
 	}
 	
 	@GetMapping("/sobre")
 	public ModelAndView pesquisar() {
-		List<Sobre> sobre = sobreBO.buscaDetalheSobre();
+		List<Sobre> sobres = sobreBO.buscaDetalheSobre();
+		
+		for (Sobre sobre : sobres) {
+			sobre = ajustaIcone(sobre);
+		}
 		
 		ModelAndView mv= new ModelAndView("pesquisaSobre");
-		mv.addObject("todosSobre", sobre);
+		mv.addObject("todosSobre", sobres);
 		return mv;
 	}
 	
+	private Sobre ajustaIcone(Sobre sobre) {
+		String icone = replaceIcone(sobre.getSobreDetalhe1().getIcone());
+		sobre.getSobreDetalhe1().setIcone(icone);
+		
+		icone = replaceIcone(sobre.getSobreDetalhe2().getIcone());
+		sobre.getSobreDetalhe2().setIcone(icone);
+		
+		icone = replaceIcone(sobre.getSobreDetalhe3().getIcone());
+		sobre.getSobreDetalhe3().setIcone(icone);
+		
+		return sobre;
+	}
+
+	private String replaceIcone(String icone) {
+		return icone.replaceAll("fa ", "");
+	}
+
 	@GetMapping("/sobre/{id}")
 	public ModelAndView edicao(@PathVariable Long id) {
 		Sobre sobre = sobreBO.buscaDetalheSobrePorId(id);
+		sobre = ajustaIcone(sobre);
 		
 		ModelAndView mv = new ModelAndView(CADASTRO_SOBRE);
 		mv.addObject(sobre);
