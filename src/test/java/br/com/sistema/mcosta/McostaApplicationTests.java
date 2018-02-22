67d7a3abcae5ff432cc26c3d723afb729f7b2436
ALAN_PC\alanl <alanllcardoso@gmail.com>
2018-02-21 22:25:26 -0300

    Teste

--- src/test/java/br/com/sistema/mcosta/McostaApplicationTests.java
+++ src/test/java/br/com/sistema/mcosta/McostaApplicationTests.java
@@ -1,6 +1,5 @@
 package br.com.sistema.mcosta;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
